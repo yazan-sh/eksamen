dc7af02c0e69717f613c57a260f30899aebbdc32
yazan-sh <64336227+yazan-sh@users.noreply.github.com>
2020-10-23 16:36:22 +0200

    løsningen på oppagve 1

--- src/no/oslomet/cs/algdat/Eksamen/EksamenSBinTre.java
+++ src/no/oslomet/cs/algdat/Eksamen/EksamenSBinTre.java
@@ -1,12 +1,9 @@
 package no.oslomet.cs.algdat.Eksamen;
 
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.Iterator;
-import java.util.StringJoiner;
+import java.util.*;
 
-public class EksamenSBinTre<T> {
+public class EksamenSBinTre<T> implements EksamenSBinTr {
     private static final class Node<T>   // en indre nodeklasse
     {
         private T verdi;                   // nodens verdi
@@ -46,6 +43,8 @@ public class EksamenSBinTre<T> {
         comp = c;
     }
 
+
+
     public boolean inneholder(T verdi) {
         if (verdi == null) return false;
 
@@ -84,11 +83,40 @@ public class EksamenSBinTre<T> {
     }
 
     public boolean leggInn(T verdi) {
-        throw new UnsupportedOperationException("Ikke kodet ennå!");
-    }
+        //throw new UnsupportedOperationException("Ikke kodet ennå!");
+            // skal ligge i class SBinTre
+        //etter at jeg kopirte koden fra5.2.4a og fikk forklart at p starter i roten og q er forelder til p
+
+            Objects.requireNonNull(verdi, "Ulovlig med nullverdier!");
+
+            Node<T> p = rot, q = null;               // p starter i roten
+            int cmp = 0;                             // hjelpevariabel
+
+            while (p != null)       // fortsetter til p er ute av treet
+            {
+                q = p;                                 // q er forelder til p
+                cmp = comp.compare(verdi,p.verdi);     // bruker komparatoren
+                p = cmp < 0 ? p.venstre : p.høyre;     // flytter p
+            }
+
+            // p er nå null, dvs. ute av treet, q er den siste vi passerte
+            // her når vi oppretter en ny node med verdi så får vi feil melding
+            //men grunnen til at q skal være er att q er den forelder til p
+            p = new Node<>(verdi,q);                   // oppretter en ny node
+
+            if (q == null) rot = p;                  // p blir rotnode
+            else if (cmp < 0) q.venstre = p;         // venstre barn til q
+            else q.høyre = p;                        // høyre barn til q
+            //og her så må vi øke endringen
+        endringer++;
+        antall++;                                // én verdi mer i treet
+            return true;                             // vellykket innlegging
+        }
+
 
     public boolean fjern(T verdi) {
         throw new UnsupportedOperationException("Ikke kodet ennå!");
+
     }
 
     public int fjernAlle(T verdi) {
